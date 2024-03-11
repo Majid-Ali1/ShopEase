9aeab7db3cce0ac4fcd61eeef3ebae4d928d18ba
Majid-Ali1 <ma8459088@gmail.com>
2024-03-11 15:19:36 +0500

    models and migrations

---
+++ app/Models/aAddress.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class aAddress extends Model
+{
+    use HasFactory;
+}
